bf631d110a02f6ca49699d0416ffa8e6f4941e73
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-28 17:50:05 +0900

    define to run & stop server by integrant

---
+++ src/picture_gallery/infrastructure/server.clj
@@ -0,0 +1,14 @@
+(ns picture-gallery.infrastructure.server
+  (:require
+   [integrant.core :as ig]
+   [ring.adapter.jetty :as jetty]
+   [taoensso.timbre :as timbre]))
+
+(defmethod ig/init-key ::server [_ {:keys [env router port]}]
+  (timbre/info "server is running in port" port)
+  (timbre/info "router is " router)
+  (jetty/run-jetty router {:port port :join? false}))
+
+(defmethod ig/halt-key! ::server [_ server]
+  (.stop server))
+
